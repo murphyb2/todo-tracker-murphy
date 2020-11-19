eb79f8c11688529f44d80877f7b41391fc9ccc1f
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2020-11-18 21:09:25 -0500

    prod build

---
+++ client/build/precache-manifest.6314feb1d71ba639e9c4856eeceb74ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380e2eb47c0e1142ff13fd5c68601b04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74292e53656591bef4af",
+    "url": "/static/css/main.6d628d03.chunk.css"
+  },
+  {
+    "revision": "905915d755372ad2df18",
+    "url": "/static/js/2.ec43f324.chunk.js"
+  },
+  {
+    "revision": "af718d80c8db395c6955adc9082cb959",
+    "url": "/static/js/2.ec43f324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74292e53656591bef4af",
+    "url": "/static/js/main.4c7fbf7b.chunk.js"
+  },
+  {
+    "revision": "df1e1a31b190e413ac80",
+    "url": "/static/js/runtime-main.005a9088.js"
+  }
+]);
